c95f93888dbedc57af0085540c0a90b0615ec037
sonyMrk <murat.kaparov29@gmail.com>
2021-08-23 16:24:26 +0600

    Dice_Roller

---
+++ app/src/main/java/com/example/edunavigation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.edunavigation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun next(view: View){
+        val intent = Intent(this, Second_Page_Dice_Roller::class.java)
+        startActivity(intent)
+    }
+}
